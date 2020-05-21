4189be6a2dbaa1eb0828ac014fa0adadd7f4120c
Mateus Santana <mateuss.dev@gmail.com>
2020-05-21 18:36:00 -0300

    refactor: remove react hot loader

--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
-import { AppContainer } from "react-hot-loader";
-import Root from "./components/Root";
+
+import Root from "./routes/root";
+
 import * as serviceWorker from "./serviceWorker";
+
 import "./styles/index.css";
 
-const root = document.getElementById("root");
-
 render(
-  <AppContainer>
+  <React.StrictMode>
     <Root />
-  </AppContainer>,
-  root
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-if (module.hot) {
-  module.hot.accept("./components/Root", () => {
-    const NewRoot = require("./components/Root").default;
-    render(
-      <AppContainer>
-        <NewRoot />
-      </AppContainer>,
-      root
-    );
-  });
-}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
