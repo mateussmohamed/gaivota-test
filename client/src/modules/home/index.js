b556b94999e0daa30ce95680c2623fd6b370f5fb
Mateus Santana <mateuss.dev@gmail.com>
2020-05-21 20:39:21 -0300

    feat: add layout in home page

--- client/src/modules/home/index.js
+++ client/src/modules/home/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-// import { logout } from "../../auth";
+import Layout from "../../components/layout";
+import LoginButton from "./components/login-button";
 
 const Home = () => {
-  // TODO: use logout when user logged in
-
-  return <>Gaivota Test 2</>;
+  return (
+    <Layout headerActions={<LoginButton />}>
+      <div className="flex mb-4">
+        <div className="w-3/4 h-64 bg-gray-400">
+          <h1>MAP</h1>
+        </div>
+        <div className="w-1/2 h-64 bg-gray-500">
+          <h1>FORM</h1>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
