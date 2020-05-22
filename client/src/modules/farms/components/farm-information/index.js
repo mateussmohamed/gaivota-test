d821a6fe683e0f51f77ed72c5056a5eb7bc20325
Mateus Santana <mateuss.dev@gmail.com>
2020-05-22 17:26:10 -0300

    refactor: remove input price and button from farm information

--- client/src/modules/farms/components/farm-information/index.js
+++ client/src/modules/farms/components/farm-information/index.js
@@ -1,15 +1,11 @@
-import React, { useState, memo } from "react";
+import React, { memo } from "react";
 import PropTypes from "prop-types";
 
 import FarmLine from "../farm-line";
 
 const FarmInformation = ({ farm }) => {
-  const [price, setPrice] = useState("");
-
-  const handlePrice = (event) => setPrice(event.target.value);
-
   return (
-    <div>
+    <>
       <FarmLine label="Name" value={farm.name} />
       <FarmLine label="Culture" value={farm.culture} />
       <FarmLine label="Variety" value={farm.variety} />
@@ -20,34 +16,9 @@ const FarmInformation = ({ farm }) => {
       />
       <FarmLine
         label="Total area"
-        value={farm.total_area * farm.yield_estimation}
+        value={`${farm.total_area * farm.yield_estimation} ha`}
       />
-      <div className="w-full flex flex-row mb-1">
-        <label
-          htmlFor="price"
-          className="text-xl text-gray-500 capitalize pr-1"
-        >
-          Price:
-        </label>
-        <input
-          className="bg-white appearance-none border-2 border-teal-700 rounded w-24 px-2 text-teal-500 focus:outline-none focus:bg-white focus:border-teal-500"
-          id="price"
-          type="text"
-          value={price}
-          onChange={handlePrice}
-        />
-      </div>
-
-      <div className="mt-6">
-        <button className="bg-teal-500 hover:bg-teal-700 text-teal-100 py-2 px-8 rounded">
-          Buy now
-        </button>
-
-        <button className="bg-teal-500 hover:bg-teal-700 text-teal-100 py-2 px-8 ml-2 rounded">
-          BID
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
