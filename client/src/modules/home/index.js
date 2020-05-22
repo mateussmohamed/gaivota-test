322a848d50ddd81c116243a84f104ecd29c1c177
Mateus Santana <mateuss.dev@gmail.com>
2020-05-22 12:43:28 -0300

    feat: add farm-map and farm-information in home

--- client/src/modules/home/index.js
+++ client/src/modules/home/index.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Layout from "../../components/layout";
 import LoginButton from "./components/login-button";
+import FarmMap from "./components/farm-map";
+import FarmInformation from "./components/farm-information";
+import FarmSelect from "./components/farm-select";
+
+import farmsData from "../../data/farms.json";
+import farmsGeoJSON from "../../data/farmsGeoJSON.json";
 
 const Home = () => {
+  const [selectedFarm, setFarm] = useState(farmsData[0].farm_id);
+
+  const farmOptions = farmsData.map((farm) => ({
+    label: farm.name,
+    value: farm.farm_id,
+  }));
+
+  const findFarm = (farm) =>
+    farm.farm_id.toString() === selectedFarm.toString();
+
+  const currentFarm = farmsData.find(findFarm);
+  const currentMap = farmsGeoJSON.find(findFarm);
+
+  const handleSelectFarm = (event) => {
+    setFarm(event.target.value);
+  };
+
   return (
     <Layout headerActions={<LoginButton />}>
-      <div className="flex mb-4">
-        <div className="w-3/4 h-64 bg-gray-400">
-          <h1>MAP</h1>
+      <div className="flex mb-4 bg-gray-200">
+        <div className="w-3/4 p-4">
+          <FarmMap geoJson={currentMap.data} />
         </div>
-        <div className="w-1/2 h-64 bg-gray-500">
-          <h1>FORM</h1>
+        <div className="w-1/2 p-4">
+          <FarmSelect
+            options={farmOptions}
+            value={selectedFarm}
+            onChange={handleSelectFarm}
+          />
+          <FarmInformation farm={currentFarm} />
         </div>
       </div>
     </Layout>
