fa509c0b895833130acfdcd7a0f64fdf6dc9b885
Mateus Santana <mateuss.dev@gmail.com>
2020-05-23 14:10:49 -0300

    feat: add graph in farm detail

--- client/src/modules/farms/pages/farm-detail.js
+++ client/src/modules/farms/pages/farm-detail.js
@@ -2,11 +2,32 @@ import React, { useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 
 import Layout from "../../../components/layout";
+import GenericChart from "../../../components/generic-chart";
+
+import { getFarm, getFarmGeoJSON } from "../../../services/farms";
+
+import chartSelector from "../../../data/chart_selector.json";
+import chartData from "../../../data/farms_ndvi.json";
 
 import FarmMap from "../components/farm-map";
 import FarmInformation from "../components/farm-information";
 
-import { getFarm, getFarmGeoJSON } from "../../../services/farms";
+const parseValue = (value) => {
+  if (typeof value === "string") {
+    return parseFloat(value.replace(",", "."));
+  }
+
+  return value;
+};
+
+const parsedChartData = chartData.map((value) => ({
+  date: new Date(value.date).toLocaleDateString("pt-BR", {
+    dateStyle: "short",
+  }),
+  ndvi_221: parseValue(value.ndvi_221),
+  ndvi_231: parseValue(value.ndvi_231),
+  ndvi_271: parseValue(value.ndvi_271),
+}));
 
 const FarmDetail = () => {
   const params = useParams();
@@ -22,7 +43,16 @@ const FarmDetail = () => {
     <Layout>
       <div className="flex mb-4 bg-gray-200">
         <div className="w-3/4 p-4">
-          <FarmMap geoJson={currentMapGeoJSON} />
+          <FarmMap geoJson={currentMapGeoJSON} height={330} />
+
+          <div className="mt-4" />
+
+          <GenericChart
+            data={parsedChartData}
+            dataValues={["ndvi_231"]}
+            dataKeys={["date"]}
+            chartSelector={chartSelector}
+          />
         </div>
         <div className="w-1/2 p-4">
           <FarmInformation farm={farm} />
@@ -47,7 +77,7 @@ const FarmDetail = () => {
             <button
               className="bg-teal-500 hover:bg-teal-700 text-teal-100 font-bold py-4 px-10 rounded"
               onClick={() => {
-                history.push(`farm/${params.farm_id}/buy`);
+                history.push(`${params.farm_id}/buy`);
               }}
             >
               Buy now
@@ -56,7 +86,7 @@ const FarmDetail = () => {
             <button
               className="bg-teal-500 hover:bg-teal-700 text-teal-100 font-bold py-4 px-10 ml-2 rounded"
               onClick={() => {
-                history.push(`farm/${params.farm_id}/offer`);
+                history.push(`${params.farm_id}/offer`);
               }}
             >
               BID
