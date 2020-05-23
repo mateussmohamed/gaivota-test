a8b0028007be30214cbcd646977bb60a7fce8c40
Mateus Santana <mateuss.dev@gmail.com>
2020-05-23 19:03:00 -0300

    feat: add input component

---
+++ client/src/components/input/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { ErrorMessage } from "react-hook-form";
+import { classNames } from "react-extras";
+
+const Input = ({
+  label,
+  name,
+  placeholder,
+  type,
+  register,
+  autoComplete,
+  validate,
+  errors,
+}) => {
+  return (
+    <div className="mb-4">
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="email"
+      >
+        {label}
+      </label>
+      <input
+        className={classNames(
+          "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+          {
+            "border-red-500": errors[name],
+          }
+        )}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        ref={register({ ...validate })}
+      />
+
+      <ErrorMessage errors={errors} name={name}>
+        {({ message }) => (
+          <p className="text-red-500 text-xs italic mt-2">{message}</p>
+        )}
+      </ErrorMessage>
+    </div>
+  );
+};
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  register: PropTypes.func,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  autoComplete: PropTypes.string,
+  placeholder: PropTypes.string,
+  validate: PropTypes.object,
+  errors: PropTypes.object,
+};
+
+Input.defaultProps = {
+  type: "text",
+};
+
+export default Input;
